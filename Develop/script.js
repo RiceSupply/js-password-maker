76f11e5776e505a33f6db206c2825b74bb4e317c
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2024-02-12 11:53:13 -0800

    Added base parameters for variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+  
+  return '';
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);

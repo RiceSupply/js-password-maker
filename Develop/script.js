fccda5df0c495f505fd55a084292adf93227e82c
Alex Le <140272444+RiceSupply@users.noreply.github.com>
2024-02-12 12:11:11 -0800

    Added ternary operators with consolelogs

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,32 @@
 // Assignment code here
+// Has to be between 8 and 128 characters
+// Can include lower, upper, numbers, and special characters
 
 function generatePassword (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
   
+  const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = "!@#$%^&*_+?><.-=";
+
+  let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowercase ? lowerCaseChars : '';
+  allowedChars += includeUppercase ? upperCaseChars : '';
+  allowedChars += includeNumbers ? numberChars : '';
+  allowedChars += includeSymbols ? symbolChars : '';
+
+  console.log(allowedChars);
+
   return '';
 }
 
-const passwordLength = 12;
+const passwordLength = 8;
 const includeLowercase = true;
-const includeUppercase = true;
-const includeNumbers = true;
-const includeSymbols = true;
+const includeUppercase = false;
+const includeNumbers = false;
+const includeSymbols = false;
 
 const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
 
